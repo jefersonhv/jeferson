a541319c51a9e1d086c84f7fdeaf6ff222df46a2
jefersonhv <jeferson.herrera@correounuvalle.edu.co>
2024-04-02 16:12:13 -0500

    aaa

---
+++ Pruebaxd/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hola putitos");
+        System.out.println("Hola putitos");
+        System.out.println("Hola putitos");
+        System.out.println("Hola putitos");
+        System.out.println("Hola putitos");
+    }
+}
